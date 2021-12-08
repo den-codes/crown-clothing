dd231689626e965aef7068a742d7410d309d91fd
Den Codes <[dencodes87@gmail.com]>
2021-12-08 10:20:44 -0400

    added preview collection, preview collection items

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './preview-collection.styles.scss';
 
+import CollectionItem from '../item-collection/item-collection.component';
+
 const CollectionPreview = ({ title, items, id }) => (
     <div className='collection-preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
             {items.filter((item, idx) => idx < 4)
-            .map(item => (
-                <div key={item.id}>{item.name}</div>
+            .map(({ id, ...otherItemProps }) => (
+                <CollectionItem key={id}{...otherItemProps} />
             ))}
         </div>
     </div>
 );
-export default CollectionPreview
+export default CollectionPreview;
